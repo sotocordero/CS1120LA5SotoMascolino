b344f397d318d989476b8d87b71f2f0a58eb7a5c
sotocordero <sotocordero@10.80.140.204>
2017-03-22 17:36:40 -0400

    Updated All of the interfaces

--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/IArea.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/IArea.java
@@ -1,5 +1,11 @@
 package edu.wmich.cs1120.la5;
 
 public interface IArea {
-
+	double getBasicEnergyCost();
+	void setBasicEnergyCost(double basicEnergyCost);
+	double getElevation();
+	void setElevation(double elevation);
+	double getRadiation();
+	void setRadiation(double radiation);
+	double calcConsumedEnergy();
 }
--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/IExpression.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/IExpression.java
@@ -1,5 +1,5 @@
 package edu.wmich.cs1120.la5;
 
 public interface IExpression {
-
+	Integer getValue();
 }
