408c15c56a11dc65af02f737873be3f01c12442a
reecemascolino <reecemascolino@wireless146-134.wireless.wmich.edu>
2017-03-29 19:40:01 -0400

    Created java doc comments for
    -HighArea
    -Subtraction

--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/HighArea.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/HighArea.java
@@ -4,6 +4,10 @@ public class HighArea extends Area{
 	HighArea(){
 		super();
 	}
+	/**
+	 * 
+	 * @return Returns the double consumedEnergy
+	 */
 	public double calcConsumedEnergy(){
 		double consumedEnergy=getBasicEnergyCost() * 4;
 		
--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/Subtraction.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/Subtraction.java
@@ -4,8 +4,11 @@ public class Subtraction implements IOperation{
 
 	
 
-
-	
+	/**
+	 * @param IExpression object
+	 * @param IExpression object
+	 * @return Returns the difference of two IExpression objects 
+	 */
 	public Integer perform(IExpression left, IExpression right) {
 		// TODO Auto-generated method stub
 		return (left.getValue()-right.getValue());
