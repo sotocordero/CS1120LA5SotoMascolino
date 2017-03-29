233ada1c522944e19b655c941361fa3766c2f68f
reecemascolino <reecemascolino@wireless146-134.wireless.wmich.edu>
2017-03-29 13:15:40 -0400

    Created literal constructor

--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/Literal.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/Literal.java
@@ -1,9 +1,12 @@
 package edu.wmich.cs1120.la5;
 
 public class Literal implements IExpression{
-private int value;
+	private int value;
+	public Literal(int value1){
+		this.value=value1;
+	}
+	
 	public Integer getValue(){
-		
 		return value;
 	}
 }
