2ff172f0c4ca6d2b0502756fd45fd2b7ddc3f9ee
reecemascolino <reecemascolino@wireless146-134.wireless.wmich.edu>
2017-03-29 13:24:54 -0400

    Finished getValue method for Binary Expression

--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/BinaryExpression.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -1,10 +1,20 @@
 package edu.wmich.cs1120.la5;
 
-public class BinaryExpression implements IExpression {
-	private int value;
-	public Integer getValue(){
+public class BinaryExpression implements IExpression{
+	private IOperation op;
+	private IExpression left;
+	private IExpression right;
+	
+	public BinaryExpression(IExpression left,IExpression right, IOperation op){
+		this.left=left;
+		this.right=right;
+		this.op=op;
 		
-		return value;
+	}
+	
+	public Integer getValue(){
+		return op.perform(left, right);
+		
 	}
 
 }
