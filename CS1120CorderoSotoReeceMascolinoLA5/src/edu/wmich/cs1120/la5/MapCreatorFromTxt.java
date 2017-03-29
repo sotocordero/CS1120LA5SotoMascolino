91ce80b8f0a0d99c1c25074d7c055c92c15eeaf1
reecemascolino <reecemascolino@wireless146-134.wireless.wmich.edu>
2017-03-29 13:30:59 -0400

    Updated MapCreatorFromTxt

--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/MapCreatorFromTxt.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/MapCreatorFromTxt.java
@@ -10,26 +10,6 @@ import edu.wmich.cs1120.la5.Area;
 import edu.wmich.cs1120.la5.HighArea;
 import edu.wmich.cs1120.la5.LowArea;
 import edu.wmich.cs1120.la5.TerrainScanner;
-
-public class MapCreatorFromTxt{
-	private TerrainScanner scanner=new TerrainScanner();
-	private Area[][] area=new Area[10][10];
-	/**
-	 * Description: scanTerrain takes incoming data and parses it to a 2x2 Area array for rover use.
-	 * This method also distinguishes different Areas into high or low areas.
-	 * @param csv- takes all incoming data and puts into an arraylist for further sorting
-	 * @param counter- an integer used to help sort the csv data into a 2x2 array
-	 */
-public void scanTerrain(String fileName, int threshold) throws IOException{
-	
-    Scanner keyboard=new Scanner(System.in);
-    ArrayList<Double> csv =new ArrayList<Double>();
-    int counter=0;
-     File textFile = new File(fileName);
-
-}	
-=======
-
 import edu.wmich.cs1120.la5.Area;
 import edu.wmich.cs1120.la5.HighArea;
 import edu.wmich.cs1120.la5.LowArea;
