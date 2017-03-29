ca5c57f06effa804e57b9d38abab3137feb3b3e0
reecemascolino <reecemascolino@wireless146-134.wireless.wmich.edu>
2017-03-29 17:46:26 -0400

    Added java doc to ElevationAnalyzer

--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/ElevationAnalyzer.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/ElevationAnalyzer.java
@@ -11,15 +11,27 @@ public class ElevationAnalyzer implements IRover{
 	private ArrayList<IArea> path = new ArrayList<IArea>();
 	private String analysis;
 	
-	
+	/**
+	 * Getter for ArrayList of IArea
+	 * @return Returns ArrayList of IArea
+	 */
 	public ArrayList<IArea> getPath(){
 			return path;
 		}
 	
+	/**
+	 * Setter for ArrayList of IArea
+	 * @param Takes an ArrayList of IArea
+	 */
 	public void setPath(ArrayList<IArea> path){
 		this.path= path;
 	}
 	
+	/**
+	 * This method finds the average elevation of the 
+	 * ArrayList path
+	 * Calls the setAnalysis method
+	 */
 	public void analyzePath(){
 		double sum = 0;
 		double average;
@@ -29,22 +41,28 @@ public class ElevationAnalyzer implements IRover{
 		average=sum/path.size();
 		setAnalysis(""+average);
 	}
-	
+	/**
+	 * getter for analysis
+	 * @return Returns String analysis
+	 */
 	public String getAnalysis(){
 		return analysis;
 	}
+	/**
+	 * setter for analysis
+	 * @param Takes a String analysis
+	 */
 	public void setAnalysis(String analysis){
 		this.analysis=analysis;
 	}
 
 	@Override
+	
 	public String toString() {
 		return "Elevation Analyzer";
 	}
 	
-	public double elevationAnalyzer(){
-		double total=0;
-		
-		return total;
-	}
+	
+	
+	
 }
