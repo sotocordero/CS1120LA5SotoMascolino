f3b6bd8b57e1cbd1c3be69de8a09bd826609b081
reecemascolino <reecemascolino@wireless146-134.wireless.wmich.edu>
2017-03-29 17:31:09 -0400

    Added java doc for addition

--- CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/Addition.java
+++ CS1120CorderoSotoReeceMascolinoLA5/src/edu/wmich/cs1120/la5/Addition.java
@@ -3,6 +3,7 @@ package edu.wmich.cs1120.la5;
 public class Addition implements IOperation{
 
 	/**
+	 * 
 	 * @param IExpression object 
 	 * @param IExpression object
 	 * @return Returns the sum of the left and right objects
